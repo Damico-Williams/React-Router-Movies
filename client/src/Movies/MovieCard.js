2315494dfd430ed699b09b7496371f942347fc06
damico williams <damicowilliams@gmail.com>
2020-01-21 16:17:55 -0800

    done

--- client/src/Movies/MovieCard.js
+++ client/src/Movies/MovieCard.js
@@ -1,7 +1,31 @@
 import React from 'react';
+import axios from 'react';
+import {useState, useEffect} from 'react';
 
 const MovieCard = props => {
-  return;
+  const [movies, setMovies] = useState([])
+ 
+  const { title, director, metascore, stars } = movies;
+  return (
+    <div className="save-wrapper">
+      <div className="movie-card">
+        <h2>{title}</h2>
+        <div className="movie-director">
+          Director: <em>{director}</em>
+        </div>
+        <div className="movie-metascore">
+          Metascore: <strong>{metascore}</strong>
+        </div>
+        <h3>Actors</h3>
+          {stars.map(star => (
+          <div key={star} className="movie-star">
+            {star}
+          </div>
+        ))}
+      </div>
+      <div className="save-button">Save</div>
+    </div>
+  );
 };
 
 export default MovieCard;
